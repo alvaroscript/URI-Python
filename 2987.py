36679b9f61367f8ce040318e5bc16eb075f6a06b
Mateus Álvaro V. Terra <60107568+alvaroscript@users.noreply.github.com>
2021-08-11 20:27:17 -0300

    Create 2987.py
    Balão de honra.

---
+++ 2987.py
@@ -0,0 +1,4 @@
+i = input()
+letras = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+n = letras.index(i)
+print(n)
